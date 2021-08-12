0abcf2779a27c0c29adc98d02c1b66f5753aa9fa
Pavel Drow <paveldrow@yandex.ru>
2021-08-13 01:16:24 +0300

    Реализовал header

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,27 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+// 'use strict';
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+// pageHeader.classList.remove('page-header--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
+
+const menuToggle = function () {
+  const pageHeader = document.querySelector('.page-header');
+  const buttonMenu = document.querySelector('.page-header__button-menu');
+
+  buttonMenu.addEventListener('click', () => {
+    pageHeader.classList.toggle('page-header--open-menu');
+  })
+}
+
+menuToggle()
+;
